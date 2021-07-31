446f6f5d750c2ef7d1dbb226d4399751af523078
lokeshKumar <lokeshkumargithub@gmail.com>
2021-08-01 00:31:54 +0530

    send message commpited

--- public/client.js
+++ public/client.js
@@ -1,4 +1,47 @@
 // var io = io("http://localhost:3000")
 
 
-io()
+const socket = io()
+
+let name;
+let message_area = document.querySelector('.message_area')
+let textarea = document.querySelector('#text_area')
+
+do {
+    name = prompt('Enter your name')
+} while (!name);
+
+
+textarea.addEventListener('keyup', (e)=>{
+    if(e.key === 'Enter'){
+        sendMessage(e.target.value)
+    }
+
+})
+
+
+function sendMessage(message){
+    // console.log(message)
+    let msg = {
+        user:name,
+        message:message
+    }
+
+    appendMessage(msg,'outgoing')
+    textarea.value = ''
+    scrollToBottom()
+
+
+}
+
+
+
+function appendMessage(msg, type){
+
+}
+
+
+function scrollToBottom(){
+    message_area.scrollTop = message_area.scrollHeight
+
+}
